77c1d3a83befc04d0a7311459bb9cf183911c0e5
ChonrevC <70560657+ChonrevC@users.noreply.github.com>
2023-09-05 23:22:21 -0500

    Add files via upload

---
+++ modulesAndPip/usefulTools.py
@@ -0,0 +1,18 @@
+# THIS IS JUST A SUPPORT FILE. It's in this folder for "modulesAndPip.py"
+
+import random
+
+feet_in_mile = 5280
+meters_in_kilometer = 1000
+beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star"]
+
+
+def get_file_ext(filename):
+    return filename[filename.index(".") + 1:]
+
+
+def roll_dice(num):
+    return random.randint(1, num)
+   
+   
+
